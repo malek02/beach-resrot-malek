06ea71548b845d18639e5398c2925f18d73d048b
Malek Mechi <malek.mechi.ext@aleia.com>
2022-06-30 14:00:03 +0100

    quick test

--- src/context.js
+++ src/context.js
@@ -1,75 +1,73 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import objit from "./data";
 const firstContext = React.createContext();
 
- class ContextProvider extends Component {
-     state={
-         allRoom:[],
-         defienRomms:[],
-         featuredRooms:[],
-         loading: true,
-         type: "all",
-     }
+class ContextProvider extends Component {
+  state = {
+    allRoom: [],
+    defienRomms: [],
+    featuredRooms: [],
+    loading: true,
+    type: "all",
+  };
 
+  componentDidMount() {
+    let rooms = this.HfrezHate(objit);
 
-     componentDidMount(){
-         let rooms = this.HfrezHate(objit);
-      
-         let featuredRooms = rooms.filter(obj=>
-             obj.featured === true
-         )
-         this.setState({
-             allRoom:rooms,
-             featuredRooms,
-             defienRomms:rooms,
-             loading:false
-         })
-     }
-getRoom=(slug)=>{
-    
-   const sluglist= this.state.allRoom.find(room=>room.slug === slug);
-   return(sluglist)
-}
-
-maleFunction=(array,value)=>{
-    let hello = array.map(item=>{
-        let hia=item.value
-        return hia
-    })
-    return hello
-    }
-
-
-     HfrezHate=(obj)=>{
-         let firstlist= obj.map(ob=>{
-             let idlist = ob.sys.id;
-             let imagelist= ob.fields.images.map(img=>
-                img.fields.file.url);
-                let room = { ...ob.fields, imagelist, idlist };
-                return(room);
-         });
-         return(firstlist);
-
-     }
-   
-      handleChange=(type)=>{
-let tempo = [...this.state.allRoom];
-if (type !== "all") {
-    tempo = tempo.filter(room => room.type === type);
+    let filterdRooms = rooms.filter((obj) => obj.featured === true);
     this.setState({
-        defienRomms: tempo
+      allRoom: rooms,
+      featuredRooms:filterdRooms,
+      defienRomms: rooms,
+      loading: false,
     });
   }
+  getRoom = (slug) => {
+    const sluglist = this.state.allRoom.find((room) => room.slug === slug);
+    return sluglist;
+  };
 
-      }
-        render() {
-        return (
-            <firstContext.Provider value={{...this.state, hepy:this.getRoom, maleFunction:this.maleFunction, handleChange: this.handleChange} }>
-                {this.props.children}
-            </firstContext.Provider>
-                
-        );
+  maleFunction = (array, value) => {
+    let hello = array.map((item) => {
+      let hia = item.value;
+      return hia;
+    });
+    return hello;
+  };
+
+  HfrezHate = (obj) => {
+    let firstlist = obj.map((ob) => {
+      let idlist = ob.sys.id;
+      let imagelist = ob.fields.images.map((img) => img.fields.file.url);
+      let room = { ...ob.fields, imagelist, idlist };
+      return room;
+    });
+    return firstlist;
+  };
+
+  handleChange = (type) => {
+    let tempo = [...this.state.allRoom];
+    if (type !== "all") {
+      tempo = tempo.filter((room) => room.type === type);
+      this.setState({
+        defienRomms: tempo,
+      });
     }
+  };
+  render() {
+    return (
+      <firstContext.Provider
+        value={{
+          ...this.state,
+          hepy: this.getRoom,
+          maleFunction: this.maleFunction,
+          handleChange: this.handleChange,
+        }}
+      >
+        {this.props.children}
+      </firstContext.Provider>
+    );
+  }
 }
-const secondContext= firstContext.Consumer;
-export {ContextProvider,firstContext,secondContext};
+const secondContext = firstContext.Consumer;
+export { ContextProvider, firstContext, secondContext };
